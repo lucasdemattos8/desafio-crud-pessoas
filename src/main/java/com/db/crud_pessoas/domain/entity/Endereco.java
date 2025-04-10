f5543d5c356a8d6356f4916ae3eeff65d20d4222
LucasMiranda <ldemattosmiranda@gmail.com>
2025-04-10 13:35:58 -0300

    feat(listar-pessoas): adiciona anotações para o atributo de Id de endereços

--- src/main/java/com/db/crud_pessoas/domain/entity/Endereco.java
+++ src/main/java/com/db/crud_pessoas/domain/entity/Endereco.java
@@ -1,6 +1,9 @@
 package com.db.crud_pessoas.domain.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -9,7 +12,10 @@ import jakarta.persistence.Table;
 @Table(name = "enderecos")
 public class Endereco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String rua;
     private Integer numero;
     private String bairro;
