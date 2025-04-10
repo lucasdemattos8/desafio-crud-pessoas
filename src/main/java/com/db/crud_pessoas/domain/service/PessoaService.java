11cdc354989ea89b74f92e8b5205b5f886c7f092
LucasMiranda <ldemattosmiranda@gmail.com>
2025-04-10 16:08:53 -0300

    feat(lista-pessoas): adiciona o método de listar pessoas ao serviço e testes

--- src/main/java/com/db/crud_pessoas/domain/service/PessoaService.java
+++ src/main/java/com/db/crud_pessoas/domain/service/PessoaService.java
@@ -1,16 +1,29 @@
 package com.db.crud_pessoas.domain.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.db.crud_pessoas.api.dto.PessoaDTO;
 import com.db.crud_pessoas.api.dto.request.pessoa.PessoaRequisicaoDTO;
+import com.db.crud_pessoas.domain.entity.Pessoa;
+import com.db.crud_pessoas.domain.repository.EnderecoRepository;
+import com.db.crud_pessoas.domain.repository.PessoaRepository;
 import com.db.crud_pessoas.domain.service.interfaces.IPessoaService;
 
 public class PessoaService implements IPessoaService {
 
+    private PessoaRepository pessoaRepository;
+    private EnderecoRepository enderecoRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository, EnderecoRepository enderecoRepository) {
+        this.pessoaRepository = pessoaRepository;
+        this.enderecoRepository = enderecoRepository;
+    }
+
     public List<PessoaDTO> listarTodasPessoas() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarTodasPessoas'");
+        List<Pessoa> listaDePessoasDominio = pessoaRepository.findAll();
+        List<PessoaDTO> listaDePessoasDTO = converterListaDeDominioParaDTO(listaDePessoasDominio);
+        return listaDePessoasDTO;
     }
 
     public PessoaDTO criarPessoa(PessoaRequisicaoDTO pessoa) {
@@ -26,6 +39,10 @@ public class PessoaService implements IPessoaService {
     public void excluirPessoa(Long id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'excluirPessoa'");
+    }
+
+    private List<PessoaDTO> converterListaDeDominioParaDTO(List<Pessoa> listaPessoas) {
+        return listaPessoas.stream().map(PessoaDTO::new).collect(Collectors.toList());
     }
     
 }
