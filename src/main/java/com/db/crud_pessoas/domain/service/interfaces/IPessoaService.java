4e9694a87ed35eec1140b136e0810396471782a7
LucasMiranda <ldemattosmiranda@gmail.com>
2025-04-11 15:39:01 -0300

    feat(paginacao): implementa paginação no serviço e endpoint de Pessoa

--- src/main/java/com/db/crud_pessoas/domain/service/interfaces/IPessoaService.java
+++ src/main/java/com/db/crud_pessoas/domain/service/interfaces/IPessoaService.java
@@ -1,12 +1,13 @@
 package com.db.crud_pessoas.domain.service.interfaces;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.db.crud_pessoas.api.dto.PessoaDTO;
 import com.db.crud_pessoas.api.dto.request.pessoa.PessoaRequisicaoDTO;
 
 public interface IPessoaService {
-    List<PessoaDTO> listarTodasPessoas();
+    Page<PessoaDTO> listarTodasPessoas(Pageable pageable);
     PessoaDTO criarPessoa(PessoaRequisicaoDTO pessoa);
     PessoaDTO atualizarPessoa(Long id, PessoaRequisicaoDTO pessoa);
     void excluirPessoa(Long id);
